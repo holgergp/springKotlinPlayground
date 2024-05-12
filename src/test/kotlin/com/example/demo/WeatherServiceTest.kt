c36e8b209f67d6217e3960508fda8cced4e0b92e
Holger Grosse-Plankermann <holgergp@gmail.com>
2024-05-12 08:46:48 +0200

    test(weatherService): fix save mock

--- src/test/kotlin/com/example/demo/WeatherServiceTest.kt
+++ src/test/kotlin/com/example/demo/WeatherServiceTest.kt
@@ -1,37 +1,55 @@
 package com.example.demo
 
 import io.mockk.every
-import io.mockk.justRun
-import io.mockk.mockk
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import java.util.Date
 
+@ExtendWith(MockKExtension::class)
 class WeatherServiceTest {
+    @MockK
+    private lateinit var weatherRepository: WeatherRepository
+
     @Test
     fun getLastNByCity() {
 
-        val weatherRepository: WeatherRepository = mockk()
-        justRun { weatherRepository.save(any()) }
-        every { weatherRepository.findByCityOrderByDateDesc("Duesseldorf") } returns listOf(Weather(20, "C", "Berlin", Date()))
+        every { weatherRepository.save<Weather>(any()) } returnsArgument 0
+
+        every { weatherRepository.findByCityOrderByDateDesc("Duesseldorf") } returns listOf(
+            Weather(
+                20,
+                "C",
+                "Berlin",
+                Date()
+            )
+        )
 
         val weatherService = WeatherService(weatherRepository)
         val weather = Weather(20, "C", "Duesseldorf", Date())
 
-        //weatherService.reportWeather(weather)
+        weatherService.reportWeather(weather)
         val lastN = weatherService.getLastNByCity("Duesseldorf", 1)
         assert(lastN.size == 1)
     }
 
     @Test
     fun getCurrentWeatherByCity() {
-        val weatherRepository: WeatherRepository = mockk()
-        justRun { weatherRepository.save(any()) }
+        every { weatherRepository.save<Weather>(any()) } returnsArgument 0
 
-        every { weatherRepository.findByCityOrderByDateDesc("Duesseldorf") } returns listOf(Weather(20, "C", "Berlin", Date()))
+        every { weatherRepository.findByCityOrderByDateDesc("Duesseldorf") } returns listOf(
+            Weather(
+                20,
+                "C",
+                "Berlin",
+                Date()
+            )
+        )
 
         val weatherService = WeatherService(weatherRepository)
         val weather = Weather(20, "C", "Duesseldorf", Date())
-        // weatherService.reportWeather(weather)
+        weatherService.reportWeather(weather)
         val currentWeather = weatherService.getCurrentWeatherByCity("Duesseldorf")
         assert(currentWeather.temperature == 20)
     }
